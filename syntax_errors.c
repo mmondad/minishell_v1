9cbbdb7f1295cc2dcb55181a90ef93a68f3a9086
mmondad <mmondad@e1r9p11.1337.ma>
2024-05-25 20:22:29 +0100

    solve a small problem

--- syntax_errors.c
+++ syntax_errors.c
@@ -6,7 +6,7 @@
 /*   By: mmondad <mmondad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 18:14:24 by mmondad           #+#    #+#             */
-/*   Updated: 2024/05/25 16:03:27 by mmondad          ###   ########.fr       */
+/*   Updated: 2024/05/25 20:19:56 by mmondad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int	stx_errors(t_info info)
 {
-	t_list *list = info.list;
+	t_list	*list;
 
+	list = info.list;
 	if (list && list->type == PIPE)
 		return (1);
 	while (list)
 	{
-		if ((list->next && list->type != WORD && list->next->type == PIPE) || !list->type)
+		if ((list->next && list->type > PIPE
+				&& list->next->type != WORD) || !list->type)
 			return (1);
 		if ((!list->next && list->type != WORD) || check_quotes(info.tmp_line))
 			return (1);
